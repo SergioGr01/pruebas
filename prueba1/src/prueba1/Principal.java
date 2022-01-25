f79b43269c616809afe59422c496bd7a265f93f1
SergioGr01 <sergioo@gmail.com>
2022-01-25 12:49:09 +0100

    Tercera validacion

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo Java-Git");
 		System.out.println("Hola mundo Java-Git");
+		System.out.println("Adios");
 	}
 
 }
